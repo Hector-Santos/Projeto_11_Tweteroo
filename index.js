eedfde8184c9e345a52bdde67316caacaa0bf0be
Hector-Santos <habs.404@gmail.com>
2022-06-20 16:50:22 -0300

    Versão final

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let user = {};
+let tweets = [
+
+];
+
+app.post("/sign-up", (req, res) => {
+
+    user = req.body;
+    console.log(user);
+    
+    res.send("ok");
+});
+
+app.post("/tweets", (req, res) => {
+
+    let tweet = {
+       username: user.username,
+       avatar: user.avatar,
+       tweet : req.body.tweet
+    }
+
+    tweets.push(tweet)
+    
+    res.send("ok");
+});
+
+app.get("/tweets", (req, res) => {
+    let resposta = tweets
+    if (tweets.length > 10)
+    resposta = tweets.slice(-10)
+
+    res.send(resposta);
+});
+
+
+app.listen(5000);
